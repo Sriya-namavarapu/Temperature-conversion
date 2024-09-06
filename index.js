98ee1e3bec25d09b8e6b7f7ea379ef084b9a53cf
Sriya-namavarapu <sriya.namavarapu@gmail.com>
2024-09-06 01:48:06 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const textbox = document.getElementById("textbox");
+const tofarenheit = document.getElementById("tofarenheit");
+const tocelsius = document.getElementById("tocelsius");
+const result = document.getElementById("result");
+let temp;
+
+
+
+function convert(){
+if(tofarenheit.checked){
+temp=Number(textbox.value);
+temp=temp*9/5+32;
+result.textContent = temp.toFixed(1) + " deg F"
+}
+else if(tocelsius.checked){
+    temp=Number(textbox.value);
+    temp=(temp-32)*(5/9);
+    result.textContent = temp.toFixed(1) + " deg C"
+}
+else{
+    result.textContent = "Please select a unit"
+}
+}
